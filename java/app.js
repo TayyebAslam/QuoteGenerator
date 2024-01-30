b00db3cbd6a24a592a5085fa8716e35e0bfebff3
TayyebAslam <tayyebaslam126@gmail.com>
2024-01-30 15:49:19 +0500

    QuoteGenerator

---
+++ java/app.js
@@ -0,0 +1,16 @@
+const quoteText = document.querySelector(".quote"),
+    quoteBtn = document.querySelector("button"),
+    authorName = document.querySelector(".author .name");
+
+function randomQuote() {
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+        console.log(result);
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.innerText = "New Quote";
+        quoteBtn.classList.remove("loading");
+    });
+}
+quoteBtn.addEventListener("click", randomQuote);
